6e13ededde301caf10776216877bab4a69269270
Bruno Coutinho Santana <116001928+brunocoutinhosantana@users.noreply.github.com>
2024-09-29 12:34:48 -0300

    Add files via upload

---
+++ validacaodedados.py
@@ -0,0 +1,7 @@
+c = str(input('Informe seu sexo: [M/F] ')).upper()
+while c != 'M' and c != 'F':
+    c = str(input('Dados inválidos. Pro favor, informe seu sexo: ')).upper()
+if c == 'M':
+    print('Sexo masculino cadastrado com sucesso!')
+else:
+    print('Sexo feminino cadastrado com sucesso!')
