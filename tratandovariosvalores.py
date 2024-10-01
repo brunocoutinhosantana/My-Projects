6090eafe9183679e2d643017938d2803005770da
Bruno Coutinho Santana <116001928+brunocoutinhosantana@users.noreply.github.com>
2024-10-01 18:40:07 -0300

    Add files via upload

---
+++ tratandovariosvalores.py
@@ -0,0 +1,8 @@
+n = cont = soma = 0
+while n != 999:
+    n = int(input('Digite um número [999 para parar]: '))
+    if n != 999:
+        cont += 1
+        soma += n
+        n += n
+print('Você digitou {} números e a soma entre eles foi {}.'.format(cont, soma))
