64461c6e682fb216aa15a8bf0f6f9195e5cab933
Bruno Coutinho Santana <116001928+brunocoutinhosantana@users.noreply.github.com>
2024-10-07 13:35:17 -0300

    Add files via upload

---
+++ maioremenorvaloremtuplas.py
@@ -0,0 +1,5 @@
+from random import randint
+sorteados = (randint(0, 10), randint(0, 10), randint(0, 10), randint(0, 10), randint(0, 10))
+print(f'Os valores sorteados foram: {sorteados}')
+print(f'O maior valor sorteado foi {max(sorteados)}')
+print(f'O menos valor sorteado foi {min(sorteados)}')
