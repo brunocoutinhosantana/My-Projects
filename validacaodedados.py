74dc3198d4da085da4e44fee2aa279c28ef1ebcc
Bruno Coutinho Santana <116001928+brunocoutinhosantana@users.noreply.github.com>
2024-11-29 16:42:57 -0300

    Delete validacaodedados.py

--- validacaodedados.py
+++
@@ -1,7 +0,0 @@
-c = str(input('Informe seu sexo: [M/F] ')).upper().strip()[0]
-while c != 'M' and c != 'F':
-    c = str(input('Dados inválidos. Pro favor, informe seu sexo: ')).upper().strip()[0]
-if c == 'M':
-    print('Sexo masculino cadastrado com sucesso!')
-else:
-    print('Sexo feminino cadastrado com sucesso!')
