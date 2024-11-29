2648ce38c09f4e21b178861d2c139218562ff912
Bruno Coutinho Santana <116001928+brunocoutinhosantana@users.noreply.github.com>
2024-11-29 16:43:19 -0300

    Delete tratandovariosvalores.py

--- tratandovariosvalores.py
+++
@@ -1,8 +0,0 @@
-n = cont = soma = 0
-while n != 999:
-    n = int(input('Digite um número [999 para parar]: '))
-    if n != 999:
-        cont += 1
-        soma += n
-        n += n
-print('Você digitou {} números e a soma entre eles foi {}.'.format(cont, soma))
