7c5772c7478a0912440cf0e4586e63e873395075
Bruno Coutinho Santana <116001928+brunocoutinhosantana@users.noreply.github.com>
2024-11-29 16:49:56 -0300

    Delete maioremenorvaloremtuplas.py

--- maioremenorvaloremtuplas.py
+++
@@ -1,5 +0,0 @@
-from random import randint
-sorteados = (randint(0, 10), randint(0, 10), randint(0, 10), randint(0, 10), randint(0, 10))
-print(f'Os valores sorteados foram: {sorteados}')
-print(f'O maior valor sorteado foi {max(sorteados)}')
-print(f'O menos valor sorteado foi {min(sorteados)}')
