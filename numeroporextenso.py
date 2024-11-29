9c3bc7422f2ab8600708c2bcde77941a2c87dd31
Bruno Coutinho Santana <116001928+brunocoutinhosantana@users.noreply.github.com>
2024-11-29 16:41:54 -0300

    Delete numeroporextenso.py

--- numeroporextenso.py
+++
@@ -1,8 +0,0 @@
-numeros = ('Zero', 'Um', 'Dois', 'Três', 'Quatro', 'Cinco', 'Seis', 'Sete', 'Oito', 'Nove', 'Dez', 'Onze', 'Doze', 'Treze', 'Quatorse', 'Quinze', 'Desesseis', 'Desessete', 'Desoito', 'Desenove', 'Vinte')
-while True:
-    num = int(input('Digite um número de 0 a 20: '))
-    if 0 <= num <= 20:
-        print(f'Você digitou o número {numeros[num]}')
-        break
-    else:
-        print('Tente novamente!', end=' ')
