5d561b384d094247f4eae2e62672f48fd2e93ce6
Bruno Coutinho Santana <116001928+brunocoutinhosantana@users.noreply.github.com>
2024-11-29 16:43:37 -0300

    Delete somaimparesmultiplosdetres.py

--- somaimparesmultiplosdetres.py
+++
@@ -1,9 +0,0 @@
-cont = 0
-s = 0
-for c in range(1, 501, 2):
-    if c % 3 == 0:
-        cont += 1
-        s += c
-print('A soma entre todos os {} números ímpares que são múltiplos de três e que se encontram no intervalo de 1 até 500 é {}!!!'.format(cont, s))
-
-
