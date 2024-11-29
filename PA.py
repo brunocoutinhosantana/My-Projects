a8fe35a21bb4797dd144b96da9c9e80f4df50e06
Bruno Coutinho Santana <116001928+brunocoutinhosantana@users.noreply.github.com>
2024-11-29 16:34:44 -0300

    Delete PA.py

--- PA.py
+++
@@ -1,9 +0,0 @@
-print('='*30)
-print('10 TERMOS DE UMA PA')
-print('='*30)
-termo = int(input('Primeiro termo: '))
-razão = int(input('Razão: '))
-decimo = termo +(10 - 1) * razão
-for c in range(termo, decimo + razão, razão):
-    print('{} -> '.format(c), end =' ')
-print('ACABOU')
