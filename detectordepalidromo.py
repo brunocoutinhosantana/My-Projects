0f886e14c620dca415e46bda3d6c5bbd648ed7de
Bruno Coutinho Santana <116001928+brunocoutinhosantana@users.noreply.github.com>
2024-11-29 16:44:41 -0300

    Delete detectordepalidromo.py

--- detectordepalidromo.py
+++
@@ -1,6 +0,0 @@
-frase = str(input('Digite uma frase: ').replace(' ', '').upper())
-print('O contrário da frase {} é {}'.format(frase, frase[::-1]))
-if frase == frase[::-1]:
-    print('A frase digitade é um palíndromo!')
-else:
-    print('A frase digitada não é um palíndromo!')
