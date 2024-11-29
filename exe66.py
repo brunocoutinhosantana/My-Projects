f728a3c8430d24c9067f7b92610574ce7f49ce8c
Bruno Coutinho Santana <116001928+brunocoutinhosantana@users.noreply.github.com>
2024-11-29 16:48:25 -0300

    Delete exe66.py

--- exe66.py
+++
@@ -1,8 +0,0 @@
-soma = cont = 0
-while True:
-    num = int(input('Digite um valor (999 para para): '))
-    if num == 999:
-        break
-    soma += num
-    cont += 1
-print(f'Foram digitados {cont} números e a soma dele é {soma}')
