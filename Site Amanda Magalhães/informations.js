8a80a7a00b0a0beb705c3a3f7d064e419ce497af
Bruno Coutinho Santana <116001928+brunocoutinhosantana@users.noreply.github.com>
2024-11-30 11:00:38 -0300

    Add files via upload

---
+++ Site Amanda Magalhães/informations.js
@@ -0,0 +1,6 @@
+let informationsJson = [
+    {id:1, name:'Do 0 ao Twerk', description: 'Um guia para iniciantes, um curso gravado, um treinamento com mais de 25 aulas para você destravar, aprender a rebolar e se sentir mais sensual.', link: 'do0aoterk.html'},
+    {id:2, name:'Segredos do Quadradinho', description: 'Um curso gravado com 11 aulas onde te ensino o quadradinho e mais 7 variações. É pra quem ainda não sabe e pra quem sabe uma variação e quer arrasar sabendo várias!', link: 'https://pay.hotmart.com/O75316402B'},
+    {id:3, name:'Aulas Ao vivo', description: 'Aulas ao vivo, toda semana para destravarmos os seus movimentos juntas e conhecer outras mulheres incríveis.', link: 'aulasOnline.html'},
+    {id:4, name:'Aulas Particulares', description: '1 hora da minha atenção só para você e todo o planejamento feito de acordo com as suas demandas', link: 'aulasParticulares.html'}
+];
